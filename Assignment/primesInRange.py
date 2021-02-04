e77167614470491dd34978e2fa58ff32acb455aa
peixiongau <70298430+peixiongau@users.noreply.github.com>
2021-02-04 13:45:05 +0800

    Add files via upload

--- Assignment/primesInRange.py
+++ Assignment/primesInRange.py
@@ -1,2 +1,32 @@
-def primesInRange():   
-    pass
+import math
+
+
+## @param low the lower bound
+## @param high the upper bound 
+def primesInRange(low = 0, high = 0)->list:
+	if not (isinstance(low, int) and isinstance(high, int)):
+		return None
+	if low < 1 or high > 1000:
+		return None
+	if low > high:
+		return None
+	result = list(range(low, high+1));
+	stop = int(math.sqrt(high))
+	time = 0;
+	for divisor in range(2, stop + 1):
+		if result == []:
+			break
+		time = int(low / divisor)
+		num = time * divisor;
+		while num <= high:
+			if time == 1:
+				time += 1
+				num = time * divisor
+				continue
+			if num in result:
+				result.remove(num)
+			time += 1
+			num = time * divisor
+	
+	return result;
+	
--- Assignment/primesInRangeTest.py
+++ Assignment/primesInRangeTest.py
@@ -1,18 +1,61 @@
+
 import unittest
-import Assignment.primesInRange as primesInRange
+from primesInRange import primesInRange
 
-class PrimeInRangeTest(unittest.TestCase):
 
+
+
+class PrimesInRangeTest(unittest.TestCase):
     def setUp(self):
         pass
-
     def tearDown(self):
         pass
-
-# Sample happy path test
-    def test100_100_ShouldDeterminePrimesInNominalRange(self):
-        lowBound = 2
-        highBound = 10
-        expectedResult = [2, 3, 5, 7]
-        actualResult = primesInRange.primesInRange(lowBound, highBound)
-        self.assertListEqual(expectedResult, actualResult)
+    
+    def test00_primesInRange(self):
+        expected = [2, 3, 5, 7]
+        actual = primesInRange(2, 10)
+        print("*Testing: Valid Inputs")
+        self.assertListEqual(expected, actual, "Test failed: Result differs!")
+        print("Test passed!")
+        
+    def test01_InvalidInput(self):
+        expected = None
+        actual = primesInRange();
+        print("*Testing: Invalid Parameters")
+        self.assertEqual(actual, expected, "Test Failed!")
+        print("Test passed!")
+        
+    def test02_InvalidLowerBound(self):
+        expected = None
+        actual = primesInRange(0, 10)
+        print("*Testing: Invalid lower bound")
+        self.assertEqual(actual,expected, "Test Failed")
+        print("Test passed!");
+        
+    def test03_InvalidBounds(self):
+        expected = None
+        actual = primesInRange(1001, 1004)
+        print("*Testing: Parameter out of range")
+        self.assertEqual(actual,expected, "Test Failed")
+        print("Test passed!")
+        
+    def test04_InvalidBounds(self):
+        expected = None
+        actual = primesInRange(998, 43)
+        print("*Testing: Lower bound greater than Upper bound")
+        self.assertEqual(actual,expected, "Test Failed")
+        print("Test passed!")
+        
+    def test05_InvalidInputFormat(self):
+        expected = None
+        actual = primesInRange('a', 'b')
+        print("*Testing: Input with invalid format")
+        self.assertEqual(actual, expected, "Test Failed")
+        print("Test passed!")
+        
+    def test06_EmptyList(self):
+        expected = []
+        actual = primesInRange(33, 34)
+        print("*Testing: Result is empty list")
+        self.assertListEqual(actual, expected, "Test Failed")
+        print("Test passed!")
